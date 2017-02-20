0eaaf1a871b7cbaacbdacfece8f22275efdc4454
Mihail Morosan <me@morosanmihail.com>
2017-02-20 22:41:07 +0000

    Some fixes to structure.

---
+++ Source/GeneticAlgorithm/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO
+            // This project is the one with the generic balancing code. It invokes sharpgenetics.
+            // It will know how to translate the specification file and invoke the bridges
+        }
+    }
+}
