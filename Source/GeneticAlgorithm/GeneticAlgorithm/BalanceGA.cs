eb9bbbfd94b89870f98d83c56ae9bb93d911880d
Mihail Morosan <me@morosanmihail.com>
2017-02-21 18:58:31 +0000

    Added basic vector evolution. Compiles and works. Keeps track of parameter max and mins and accuracies. Requires patch to sharp genetics (to load json).

---
+++ Source/GeneticAlgorithm/GeneticAlgorithm/BalanceGA.cs
@@ -0,0 +1,180 @@
+﻿using SharpGenetics.BaseClasses;
+using SharpGenetics.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithm.GeneticAlgorithm
+{
+    [DataContract]
+    class BalanceGA : PopulationMember
+    {
+        PopulationManager<BalanceGA, int, double> Manager;
+
+        [DataMember]
+        public String CreatedBy = "";
+
+        public BalanceGA(PopulationManager<BalanceGA, int, double> Manager, List<double> root = null, CRandom rand = null)
+        {
+            UpdatedAtGeneration = -1;
+
+            if (rand != null)
+            {
+                this.rand = new CRandom(rand.Next());
+            }
+
+            ReloadParameters(Manager);
+
+            Vector = root;
+
+            //Generate random one
+            if (root == null)
+            {
+                Vector = GenerateData();
+                CreatedBy = "Random";
+            }
+        }
+
+        public override void ReloadParameters<T, I, O>(PopulationManager<T, I, O> Manager)
+        {
+            this.Manager = Manager as PopulationManager<BalanceGA, int, double>;
+        }
+
+        List<double> GenerateData()
+        {
+            int Length = (int)(double)Manager.GetParameters().GetParameter("Par_Length");
+            List<double> data = new List<double>();
+
+            List<double> RangeMax = (List<double>)Manager.GetParameters().GetParameter("Params_MaxRange");
+            List<double> RangeMin = (List<double>)Manager.GetParameters().GetParameter("Params_MinRange");
+            List<int> Accuracy = (List<int>)Manager.GetParameters().GetParameter("Params_Accuracy");
+
+            for (int i = 0; i < Length; i++)
+            {
+                data.Add(GetValInRange(RangeMin[i], RangeMax[i], Accuracy[i]));
+            }
+
+            return data;
+        }
+
+        double GetValInRange(double ValueRangeMin, double ValueRangeMax, int ValueRangeAccuracy)
+        {
+            return Math.Round(rand.Next((int)(ValueRangeMin * ValueRangeAccuracy), (int)(ValueRangeMax * ValueRangeAccuracy)) / (float)ValueRangeAccuracy, (int)Math.Log10(ValueRangeAccuracy));
+        }
+
+        public override double CalculateFitness<T, Y>(int CurrentGeneration, params GenericTest<T, Y>[] values)
+        {
+            int RecalculateAfterAGeneration = 0; //TODO
+            if (this.Fitness < 0 || RecalculateAfterAGeneration > 0)
+            {
+                if (Fitness < 0)
+                {
+                    this.CreatedAtGeneration = CurrentGeneration;
+                }
+
+                if (this.UpdatedAtGeneration < CurrentGeneration)
+                {
+                    Fitness = RunGames(CurrentGeneration);
+                    this.UpdatedAtGeneration = CurrentGeneration;
+                }
+            }
+
+            return this.Fitness;
+        }
+
+        double RunGames(int CurrentGeneration)
+        {
+            double finalResult = 0;
+
+            //TODO
+
+            return finalResult;
+        }
+
+        public override T Crossover<T>(T b)
+        {
+            BalanceGA CWith = ((BalanceGA)(object)b);
+
+            var newData = new List<double>(Vector);
+
+            int Length = Vector.Count;
+
+            if (Length == 1)
+            {
+                newData[0] = (Vector[0] + CWith.Vector[0]) / 2;
+            }
+            else
+            {
+
+                int p1 = rand.Next(Length - 1);
+                int p2 = rand.Next(p1+1, Length);
+
+                for (int i = p1; i < p2; i++)
+                {
+                    newData[i] = CWith.Vector[i];
+                }
+            }
+
+            PopulationMember ret = (T)Activator.CreateInstance(typeof(T), new object[] { Manager, newData, rand });
+
+            ((BalanceGA)ret).CreatedBy = "Crossover";
+
+            return (T)ret;
+        }
+
+        public override T Mutate<T>()
+        {
+            var newData = new List<double>(Vector);
+
+            List<double> RangeMax = (List<double>)Manager.GetParameters().GetParameter("Params_MaxRange");
+            List<double> RangeMin = (List<double>)Manager.GetParameters().GetParameter("Params_MinRange");
+            List<int> Accuracy = (List<int>)Manager.GetParameters().GetParameter("Params_Accuracy");
+
+            double MutationChance = (double)Manager.GetParameters().GetParameter("extra_MutationChance");
+            
+            for (int i = 0; i < Vector.Count; i++)
+            {
+                double MutateRange = (double)(RangeMax[i] - RangeMin[i]) / Accuracy[i];
+                if (rand.Next(0, 100) <= MutationChance * 100)
+                {
+                    double Shift = Math.Round(rand.NextDouble(-MutateRange, MutateRange), (int)Math.Log10(Accuracy[i]));
+                    newData[i] += Shift;
+                    if (newData[i] > RangeMax[i])
+                        newData[i] = RangeMax[i];
+                    if (newData[i] < RangeMin[i])
+                        newData[i] = RangeMin[i];
+                }
+            }
+
+            PopulationMember ret = new BalanceGA(Manager, newData, rand);
+
+            ((BalanceGA)ret).CreatedBy = "Mutation";
+
+            return (T)ret;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(",", Vector);
+        }
+
+        public override PopulationMember Clone()
+        {
+            BalanceGA ret = new BalanceGA(Manager, new List<double>(Vector), rand);
+            return ret;
+        }
+
+        public override double GetFitness()
+        {
+            return this.Fitness;
+        }
+
+        public override PopulationManager<T, I, O> GetParentManager<T, I, O>()
+        {
+            return Manager as PopulationManager<T, I, O>;
+        }
+    }
+}
--- Source/GeneticAlgorithm/Program.cs
+++ Source/GeneticAlgorithm/Program.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using GeneticAlgorithm.GeneticAlgorithm;
+using SharpGenetics.BaseClasses;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace GeneticAlgorithm
 {
@@ -13,6 +18,145 @@ namespace GeneticAlgorithm
             //TODO
             // This project is the one with the generic balancing code. It invokes sharpgenetics.
             // It will know how to translate the specification file and invoke the bridges
+            bool DoRun = true;
+            do
+            {
+                try
+                {
+                    GPRunManager<BalanceGA, int, double> RunManager = null;
+
+                    List<GenericTest<int, double>> tests = new List<GenericTest<int, double>>();
+
+                    GenericTest<int, double> test = new GenericTest<int, double>();
+                    test.AddInput("table", 1);
+                    tests.Add(test);
+
+                    string BatchRun = "../RunParams/BatchRun.xml";
+
+                    if (args.Count() > 0)
+                    {
+                        BatchRun = args[0];
+                    }
+                    else
+                    {
+                        //List all batch files in RunParams
+                        var ListOfBatch = Directory.EnumerateFiles("../RunParams/", "Batch*.xml").ToList();
+                        for (int i = 0; i < ListOfBatch.Count(); i++)
+                        {
+                            Console.WriteLine(i + ": " + ListOfBatch[i]);
+                        }
+                        //Select one
+                        int Selection = int.Parse(Console.ReadLine());
+
+                        if (Selection < ListOfBatch.Count)
+                            BatchRun = ListOfBatch[Selection];
+                    }
+
+                    XmlDocument doc1 = new XmlDocument();
+                    doc1.Load(BatchRun);
+
+                    var classNodes = doc1.SelectNodes("/batch/run");
+
+                    for (int i = 0; i < classNodes.Count; i++)
+                    {
+                        var ParamFile = classNodes[i].Attributes["paramfile"].Value;
+                        var OutputFolder = classNodes[i].Attributes["outputfolder"].Value;
+                        var RunCount = int.Parse(classNodes[i].Attributes["runs"].Value);
+                        var GenToRun = int.Parse(classNodes[i].Attributes["generations"].Value);
+                        var RandomSeed = int.Parse(classNodes[i].Attributes["randomseed"].Value);
+
+
+                        for (int Run = 1; Run <= RunCount; Run++)
+                        {
+                            DoRun = true;
+                            var Folder = "Results/" + OutputFolder + "/Run" + Run;
+                            var GensToRun = GenToRun;
+
+                            if (Directory.Exists(Folder))
+                            {
+                                var rawentries = Directory.GetFiles(Folder, "*.xml");
+
+                                if (rawentries.Count() < GenToRun)
+                                {
+                                    var entries = rawentries.OrderBy(t => t);
+
+                                    var BackupFilename = entries.Last();
+
+                                    FileStream fs = new FileStream(BackupFilename, FileMode.Open);
+
+                                    XmlDictionaryReader reader =
+                                        XmlDictionaryReader.CreateTextReader(fs, new XmlDictionaryReaderQuotas());
+                                    DataContractSerializer ser = new DataContractSerializer(typeof(GPRunManager<BalanceGA, int, double>));
+
+                                    // Deserialize the data and read it from the instance.
+                                    RunManager =
+                                        (GPRunManager<BalanceGA, int, double>)ser.ReadObject(reader, true);
+
+                                    //RunManager.ReloadParameters();
+
+                                    reader.Close();
+                                    fs.Close();
+
+                                    GensToRun = GenToRun - entries.Count();
+                                }
+                                else
+                                {
+                                    DoRun = false;
+                                }
+                            }
+                            else
+                            {
+                                RunManager = new GPRunManager<BalanceGA, int, double>(ParamFile, tests, RandomSeed * Run);
+
+                                RunManager.InitRun();
+                            }
+
+                            if (DoRun)
+                            {
+                                //System.Console.WriteLine("How many generations to run?");
+                                //GenToRun = int.Parse(Console.ReadLine());
+
+                                int res = 0;
+
+                                for (int Gener = 0; Gener < GensToRun; Gener++)
+                                {
+                                    res = RunManager.StartRun(1);
+
+                                    int c = 0;
+                                    foreach (BalanceGA FN in RunManager.GetBestMembers())
+                                    {
+                                        Console.WriteLine("Population " + c + " - " + FN + " - " + FN.Fitness);
+                                        c++;
+                                    }
+
+                                    Directory.CreateDirectory(Folder);
+
+                                    string filename = Folder + "/Backup_" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss-ffffff") + ".xml";
+                                    //FileStream stream = File.Open(filename, FileMode.Create);
+
+                                    var serializer = new DataContractSerializer(typeof(GPRunManager<BalanceGA, int, double>));
+
+                                    var settings = new XmlWriterSettings { Indent = true };
+
+                                    //serializer.WriteObject(stream, RunManager);
+                                    using (var w = XmlWriter.Create(filename, settings))
+                                    {
+                                        serializer.WriteObject(w, RunManager);
+                                    }
+
+                                    Console.WriteLine("Saving to file done");
+
+                                    //stream.Close();
+                                }
+                            }
+                        }
+                    }
+                }
+                catch(Exception e)
+                {
+
+                }
+            } while (DoRun);
         }
     }
 }
