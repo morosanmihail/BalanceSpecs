b5b305e3df5a6b84bf82d9f9c63f0b3064776940
Mihail Morosan <me@morosanmihail.com>
2017-04-05 00:54:25 +0100

    Preliminary work on exposing important properties from predictors.

---
+++ Source/BalanceSpecsGUI/Converters/PredictorTypeConverter.cs
@@ -0,0 +1,30 @@
+﻿using SharpGenetics.Predictor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace BalanceSpecsGUI.Converters
+{
+    public class PredictorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (values == null)
+                return null;
+
+            if (values[0] is string)
+                return PredictorHelper.GetParametersRequired((string)values[0]);
+            else
+                return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
