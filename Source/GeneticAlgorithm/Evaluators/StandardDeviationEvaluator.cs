4509a176ccf294f8a4fec273353202417a981cfa
Mihail Morosan <me@morosanmihail.com>
2017-06-19 11:10:22 +0100

    Added another evaluator - for comparing the standard deviation of a list of values to a target.

---
+++ Source/GeneticAlgorithm/Evaluators/StandardDeviationEvaluator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithm.Evaluators
+{
+    public class StandardDeviationEvaluator : MetricEvaluator
+    {
+        public double Evaluate(List<double> Metric, double Target)
+        {
+            double ret = 0;
+            if (Metric.Count() > 0)
+            {
+                //Compute the Average      
+                double avg = Metric.Average();
+                //Perform the Sum of (value-avg)_2_2      
+                double sum = Metric.Sum(d => Math.Pow(d - avg, 2));
+                //Put it all together      
+                ret = Math.Sqrt((sum) / (Metric.Count() - 1));
+            }
+            return Math.Abs(ret - Target);
+        }
+
+        public double Evaluate(double Metric, double Target)
+        {
+            return Math.Abs(Metric - Target);
+        }
+    }
+}
